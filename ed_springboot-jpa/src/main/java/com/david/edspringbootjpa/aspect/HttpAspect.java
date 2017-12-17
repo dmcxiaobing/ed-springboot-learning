aad77c85275a6e3f63f6412c482b9eace582206a
qq986945193 <986945193@qq.com>
2017-12-17 15:38:30 +0800

    commit aop

---
+++ ed_springboot-jpa/src/main/java/com/david/edspringbootjpa/aspect/HttpAspect.java
@@ -0,0 +1,17 @@
+package com.david.edspringbootjpa.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * aop介绍。简单使用
+ * @Author ：David
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
+//面向切面编程
+@Aspect
+@Component
+public class HttpAspect {
+
+}
