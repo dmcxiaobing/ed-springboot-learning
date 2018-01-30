9a87f5b2192dcc0ab9549bf5453eb3b0afb03830
qq986945193 <986945193@qq.com>
2018-01-30 15:01:07 +0800

    添加爬取区域表的功能

--- ed-springboot-crawler/src/main/java/com/david/crawler/http/HttpclientJsoup.java
+++ ed-springboot-crawler/src/main/java/com/david/crawler/http/HttpclientJsoup.java
@@ -1,5 +1,12 @@
 package com.david.crawler.http;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
@@ -12,8 +19,13 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 
+import com.david.crawler.utils.JdbcUtils;
+
+
+
 /**
  * 使用httpclient与jsoup结合爬取数据
+ * 
  * @author ：David
  * @weibo ：http://weibo.com/mcxiaobing
  * @github: https://github.com/QQ986945193
@@ -23,31 +35,139 @@ public class HttpclientJsoup {
 	/**
 	 * 使用httpclient与jsoup结合爬取数据
 	 */
-	@Test
-	public void testHttpClinetJsoup() throws Exception{
+	// @Test
+	public void testHttpClinetJsoup() throws Exception {
 		// 创建HttpClient对象
 		HttpClient httpClient = new DefaultHttpClient();
 		// 设置响应时间，读取源码时间。代理服务器等
 		httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 10000)
-		//.setParameter(ConnRouteParams.DEFAULT_PROXY, new HttpHost("dailiip"))
-		.setParameter(CoreConnectionPNames.SO_TIMEOUT, 10000);
+				// .setParameter(ConnRouteParams.DEFAULT_PROXY, new HttpHost("dailiip"))
+				.setParameter(CoreConnectionPNames.SO_TIMEOUT, 10000);
 		// 使用get请求，创建请求对象
 		HttpGet httpGet = new HttpGet(DavidJsoup.URL);
 		// 发送请求，获取响应数据
 		HttpResponse response = httpClient.execute(httpGet);
 		// 将网页实体转换成字符串
-		String content = EntityUtils.toString(response.getEntity(),"UTF-8");
-		
+		String content = EntityUtils.toString(response.getEntity(), "UTF-8");
+
 		// jsoup负责解析网页。
 		Document document = Jsoup.parse(content);
 		// 使用元素选择器获取网页内容
-		//根据标签名称，class名称。a标签，获取到超链接的地址和名字。。指定标签下的a
+		// 根据标签名称，class名称。a标签，获取到超链接的地址和名字。。指定标签下的a
 		Elements elements = document.select("tr.js-navigation-item a");
 		// 循环遍历元素
 		for (Element element : elements) {
-			System.out.println(element.text()+":"+element.attr("href"));
+			System.out.println(element.text() + ":" + element.attr("href"));
 		}
-		
-		
+
+	}
+
+	/**
+	 * 使用httpclient与jsoup结合爬取数据
+	 */
+	@Test
+	public void testHttpClinetJsoupGov() throws Exception {
+		// 创建HttpClient对象
+		HttpClient httpClient = new DefaultHttpClient();
+		// 设置响应时间，读取源码时间。代理服务器等
+		httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 10000)
+				// .setParameter(ConnRouteParams.DEFAULT_PROXY, new HttpHost("dailiip"))
+				.setParameter(CoreConnectionPNames.SO_TIMEOUT, 10000);
+		// 使用get请求，创建请求对象
+		HttpGet httpGet = new HttpGet("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/31/3101.html");
+//		HttpGet httpGet = new HttpGet("http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2016/12/1201.html");
+		// 发送请求，获取响应数据
+		HttpResponse response = httpClient.execute(httpGet);
+		// 将网页实体转换成字符串
+		String content = EntityUtils.toString(response.getEntity(), "gb2312");
+
+		// jsoup负责解析网页。
+		Document document = Jsoup.parse(content);
+		// 使用元素选择器获取网页内容
+		// 根据标签名称，class名称。a标签，获取到超链接的地址和名字。。指定标签下的a
+		Elements elements = document.select("tr.countytr a");
+		List<String> numbersList = new ArrayList<String>();
+		List<String> StringList = new ArrayList<String>();
+		// 循环遍历元素
+		for (Element element : elements) {
+			// System.err.println(element.text() + "");
+			if (isNumber(element.text())) {
+				numbersList.add(element.text());
+			} else {
+				StringList.add(element.text());
+			}
+
+		}
+		for (int i = 0; i < numbersList.size(); i++) {
+			fun2(numbersList, StringList,i);
+		}
+//		for (String string : numbersList) {
+//			System.err.println(string);
+//		}
+		// 120101000000
+		// 120102000000
+		// 120103000000
+		// 120104000000
+		// 120105000000
+		// 120106000000
+		// 120110000000
+		// 120111000000
+		// 120112000000
+		// 120113000000
+		// 120114000000
+		// 120115000000
+		// 120116000000
+		// 120117000000
+		// 120118000000
+		// 120119000000
+
+		// 南开区
+		// 河北区
+		// 红桥区
+		// 东丽区
+		// 西青区
+		// 津南区
+		// 北辰区
+		// 武清区
+		// 宝坻区
+		// 滨海新区
+		// 宁河区
+		// 静海区
+		// 蓟州区
+		// for (String string : StringList) {
+		// System.err.println(string);
+		// }
+
+		//
+	}
+
+	// 规范化 执行 查询操作
+	public void fun2(List<String> numbers,List<String> names,int i) throws Exception {
+		Connection connection = JdbcUtils.getConnection();
+		String sql = "insert into tb_city values(?,?)";
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+//		for (int i = 0; i < numbers.size(); i++) {
+			preparedStatement.setString(1,names.get(i));
+			preparedStatement.setString(2, numbers.get(i));
+			// 添加批处理，这一组参数就保存到集合中了。
+			preparedStatement.addBatch();
+//		}
+		long start = System.currentTimeMillis();
+		preparedStatement.execute();// 执行批处理
+		System.out.println("操作成功");
+//		long end = System.currentTimeMillis();
+//		System.out.println(end - start);
+
+	}
+
+	public static boolean isHanZi(char ch) {
+		// 判断是否汉字
+		return (ch >= 0x4E00 && ch <= 0x9FA5);
+	}
+
+	public static boolean isNumber(String str) {
+		// 判断是否纯数字
+		boolean result = str.matches("[0-9]+");
+		return result;
 	}
 }
---
+++ ed-springboot-crawler/src/main/java/com/david/crawler/utils/JdbcUtils.java
@@ -0,0 +1,69 @@
+package com.david.crawler.utils;
+
+/**
+ * @Author ：程序员小冰
+ * @GitHub: https://github.com/QQ986945193
+ */
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+/**
+ * 最简单的一个jdbc的封装工具类
+ */
+public class JdbcUtils {
+	private static Properties properties;
+
+	/**
+	 * 只在jdbcutils类加载时执行一次
+	 */
+	static {
+		try {
+			// 给properties进行初始化，即加载dbconfig.properties文件
+			InputStream iStream = JdbcUtils.class.getClassLoader().getResourceAsStream("dbconfig.properties");
+			properties = new Properties();
+			properties.load(iStream);
+			// 加载驱动类
+			Class.forName(properties.getProperty("driverClassName"));
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	/**
+	 * 获取connection连接
+	 * 
+	 * @throws SQLException
+	 */
+	public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException {
+		return DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("username"),
+				properties.getProperty("password"));
+	}
+
+	// 使用默认的配置文件，要求必须给出c3p0-config.xml
+	private static ComboPooledDataSource dataSource = new ComboPooledDataSource();
+
+	/**
+	 * 使用连接池返回一个connection连接对象
+	 * 
+	 * @throws SQLException
+	 */
+	public static Connection getC3P0Connection() throws SQLException {
+		return dataSource.getConnection();
+	}
+
+	/**
+	 * 返回连接池对象
+	 */
+	public static DataSource getDataSource() {
+		return dataSource;
+	}
+}
