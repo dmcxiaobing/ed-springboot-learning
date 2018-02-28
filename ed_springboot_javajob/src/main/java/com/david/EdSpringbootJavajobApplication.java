e97cef64f74362cfe815b940966fba47f10f30bc
qq986945193 <986945193@qq.com>
2018-02-28 10:50:01 +0800

    添加一个单例设计模式

---
+++ ed_springboot_javajob/src/main/java/com/david/EdSpringbootJavajobApplication.java
@@ -0,0 +1,17 @@
+package com.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 主要写一些常用的面试代码。
+ * @author ：David
+ * @weibo ：http://weibo.com/mcxiaobing
+ * @github: https://github.com/QQ986945193
+ */
+@SpringBootApplication
+public class EdSpringbootJavajobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdSpringbootJavajobApplication.class, args);
+	}
+}
