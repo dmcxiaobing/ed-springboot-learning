d6e54e83368d0ef00409ff352cd1ac10c4a4b2ce
qq986945193 <986945193@qq.com>
2018-03-17 10:38:53 +0800

    判断字符串

--- ed_springboot_javajob/src/main/java/com/david/introduce/StringIntroduce.java
+++ ed_springboot_javajob/src/main/java/com/david/introduce/StringIntroduce.java
@@ -10,6 +10,8 @@ public class StringIntroduce {
 		String string = "hello building 小区 3 room";
 		// 获取小区出现的位置
 		System.out.println(string.indexOf("小区"));
+		// 是否包含字符串
+		System.out.println(string.contains("小区"));
 		// 截取小区之前的数据
 		string = string.substring(0,string.indexOf("小区"));
 		System.out.println(string);
