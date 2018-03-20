28210089d1721adfb04c736250367c492e84e749
qq986945193 <986945193@qq.com>
2018-03-20 14:47:29 +0800

    添加shiro的功能

--- david_springboot_parent/david_springboot_shiro/src/main/java/com/david/web/controller/UserInfoController.java
+++ david_springboot_parent/david_springboot_shiro/src/main/java/com/david/web/controller/UserInfoController.java
@@ -1,10 +1,12 @@
 package com.david.web.controller;
 
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * 用户信息
+ * 
  * @author ：David
  * @weibo ：http://weibo.com/mcxiaobing
  * @github: https://github.com/QQ986945193
@@ -13,7 +15,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/userInfo")
 public class UserInfoController {
 
-//	public String userInfo() {
-//		
-//	}
+	/**
+	 * 用户查询
+	 */
+	@RequestMapping("/userList")
+	@RequiresPermissions("userInfo:view") // 权限管理
+	public String userInfo() {
+		return "userInfo";
+	}
+
+	/**
+	 * 用户添加
+	 */
+	@RequestMapping("/userAdd")
+	@RequiresPermissions("userInfo:add")
+	public String userInfoAdd() {
+		return "userInfoAdd";
+	}
+
+	/**
+	 * 用户删除
+	 */
+	@RequestMapping("/userDel")
+	@RequiresPermissions("userInfo:del")
+	public String userDel() {
+		return "userInfoDel";
+	}
 }
