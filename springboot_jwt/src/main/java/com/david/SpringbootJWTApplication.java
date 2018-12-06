4554dbe83ee8bbf8ceb00bc136554dfba450b5a1
David <986945193@qq.com>
2018-12-06 11:08:09 +0800

    init jwt project

---
+++ springboot_jwt/src/main/java/com/david/SpringbootJWTApplication.java
@@ -0,0 +1,14 @@
+package com.david;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 使用jetty启动项目
+ */
+@SpringBootApplication
+public class SpringbootJWTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJWTApplication.class, args);
+	}
+}
---
+++ springboot_jwt/src/main/java/com/david/web/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.david.web.controller;
+
+import com.david.entity.User;
+import com.david.utils.JwtUtil;
+import com.david.utils.ResponseData;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    @ResponseBody
+    public ResponseData login(@RequestParam String username, @RequestParam String password) {
+        if ("xiaoli".equals(username) && "123456".equals(password)) {
+            ResponseData responseData = ResponseData.ok();
+            User user = new User();
+            user.setId(1);
+            user.setUsername(username);
+            user.setPassword(password);
+            responseData.putDataValue("user", user);
+//            String token = JwtUtil.sign(user, 30L * 24L * 3600L * 1000L);
+            String token = JwtUtil.sign(user, 10000);
+            if (token != null) {
+                responseData.putDataValue("token", token);
+            }
+            return responseData;
+        }
+        return ResponseData.customerError().putDataValue(ResponseData.ERRORS_KEY, new String[] { "用户名或者密码错误" });
+    }
+
+    @GetMapping("/getUser")
+    @ResponseBody
+    public ResponseData getInfo(@RequestParam String token) {
+        User user = JwtUtil.unsign(token, User.class);
+        if (user != null) {
+            return ResponseData.ok().putDataValue("user", user);
+        }
+        return ResponseData.customerError().putDataValue(ResponseData.ERRORS_KEY, new String[] { "token不合法" });
+    }
+}
